2b0260cd1ac3ece17206afb13dade331b96bd86b
Shanaya <shanaya.kumari139877@marwadiuniversity.ac.in>
2026-07-31 15:10:17 +0530

    Add files via upload

---
+++ ep_u1p1.java
@@ -0,0 +1,13 @@
+//multiplication yable upto 10.
+public class ep_u1p1
+{
+	public static void main(string args[])
+	{
+		int i;;
+		system.out.prfloatln("multipilication table of 10");
+		for (i=1;i<=10;i++)
+		{ system.out.prfloatln("10*"+i+" = "+(10*i));
+		}
+	}
+}
+		
